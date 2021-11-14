d601ce4956acee043bd71b038343ebcde5361a3b
AdithyaSanyal <adithyasanyal@gmail.com>
2021-11-15 00:19:07 +0530

    Added the news section

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { cryptoApi } from "../services/cryptoAPI";
+import { cryptoNews } from "../services/cryptoNews";
 
 export default configureStore({
   reducer: {
     [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNews.reducerPath]: cryptoNews.reducer,
   },
 });
--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
-import { Link } from "react-router-dom";
+import { useGetCryptoNewsQuery } from "../services/cryptoNews";
+import { useGetCryptosQuery } from "../services/cryptoAPI";
 
-const News = () => {
+const demoImage =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState("Cryptocurrency");
+  const { data } = useGetCryptosQuery(100);
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: newsCategory,
+    count: simplified ? 6 : 24,
+  });
+  console.log(cryptoNews);
+  if (!cryptoNews?.value) return <div>Loading...</div>;
+
   return (
-    <div>
-      <h1>News</h1>
-    </div>
+    <Row gutter={[24, 24]}>
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) =>
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            }
+          >
+            <Select.Option value="Cryptocurrency">Cryptocurrency</Select.Option>
+            {data?.data?.coins.map((coin) => (
+              <Select.Option value={coin.name}>{coin.name}</Select.Option>
+            ))}
+          </Select>
+        </Col>
+      )}
+      {cryptoNews.value.map((news, index) => (
+        <Col xs={24} sm={12} lg={8} key={index}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Typography.Title level={4} className="news-title">
+                  {news.name}
+                </Typography.Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt=""
+                  width="150"
+                  height="100"
+                />
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImage
+                    }
+                  />
+                  <Typography.Text className="provider-name">
+                    {news.provider[0]?.name}
+                  </Typography.Text>
+                </div>
+                <Typography.Text>
+                  {"   " + moment(news.datePublished).startOf("ss").fromNow()}
+                </Typography.Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
   );
 };
 
